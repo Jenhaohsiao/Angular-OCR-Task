bc576766322ea623af0e7ea2d46317d6a4812cbc
Jenhao Hsiao <jenhao.hsiao@gmail.com>
2019-06-06 00:31:15 -0400

    Added S3, Tesseract, SDKs

--- angular-ocr-task/src/app/image-ocr/image-ocr.component.ts
+++ angular-ocr-task/src/app/image-ocr/image-ocr.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { TesseractWorker } from 'tesseract.js';
+import { S3UploaderService } from 'src/app/services/s3-uploader.service';
+
 
 @Component({
   selector: 'app-image-ocr',
@@ -7,9 +10,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ImageOcrComponent implements OnInit {
 
-  constructor() { }
+
+  public selectedFiles: FileList;
+  public selectedFileName: string = "Choose a file";
+  public selectedPath: String;
+
+  public inputedImage = new Image();
+  public inputedImageURL = "https://fakeimg.pl/350x350/282828/EAE0D0/?text= No Image"
+  public ocrResult;
+  public SDKInitPerce;
+  public progressPerce;
+
+
+  constructor(
+    private S3UploaderService: S3UploaderService,
+  ) { }
 
   ngOnInit() {
+    this.setInputedImageURL(this.inputedImageURL)
+  }
+
+  selectFile(event) {
+    this.selectedFileName = event.target.files[0].name;
+    this.selectedFiles = event.target.files;
+  }
+
+  async uploadImage() {
+    const file = this.selectedFiles.item(0);
+    const URL = await this.S3UploaderService.uploadFile(file);
+    this.setInputedImageURL(URL);
+    console.log("After upload, URL:", URL);
+
+  }
+
+  setInputedImageURL(_url) {
+    this.inputedImage.src = _url;
+
+  }
+
+
+  ocrImage() {
+
+    const worker = new TesseractWorker();
+
+    worker.recognize(this.inputedImage)
+      .progress(progress => {
+        console.log('progress', progress);
+        this.progressPerce = progress.progress * 100;
+      }).then(result => {
+        console.log('result', result);
+
+        this.ocrResult = result.text
+        console.log('this.ocrResult:', this.ocrResult)
+
+      });
   }
 
 }
